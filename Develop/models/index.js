44ed8b174ec526cc686fc15a38f296ff6f57cfbc
joshua <joshuacrawford8877@gmail.com>
2023-09-24 17:39:26 -0400

    d

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -18,14 +18,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tags',
+  //as: 'tags',//
   foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'products',
+  //as: 'products',//
   foreignKey: 'tag_id',
 });
 
